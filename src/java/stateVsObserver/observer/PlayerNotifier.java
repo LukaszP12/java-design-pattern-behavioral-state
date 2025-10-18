acba2bf09b67aba9bbbd9846cb7d1b4b617a325f
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-18 16:50:25 +0200

    - Observers react to changes.
    - Sender doesn’t know who is listening.

--- src/java/stateVsObserver/observer/PlayerNotifier.java
+++ src/java/stateVsObserver/observer/PlayerNotifier.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerNotifier {
+    // Yes, Notifier knows it has observers, but it does NOT depend on their concrete types.
+    // So it's still loosely coupled — it only knows them as PlayerObserver objects, not who they really are.
     private List<PlayerObserver> observers = new ArrayList<>();
 
     public void addObserver(PlayerObserver observer) {
